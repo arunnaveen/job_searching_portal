541112a599261b942c4e8d5d0b8c18203cd8ebb1
arunnaveen <subashnasa1662@gmail.com>
2024-07-27 22:42:36 +0530

    connect to database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const cors = require("cors");
-const dotenv = require("dotenv").config()
+const dotenv = require("dotenv").config();
+const connectDB = require("./config/db");
 
 const app = express();
 app.use(cors());
+
+//connect mongoDb
+connectDB();
 
 app.get('/',(req,res)=>{
     res.send("Server is started")
